7a62a1b4db0737ca01a68af1b6c2948d85a760e8
Jan Simunek <jan.simunek@ys.cz>
2015-01-17 17:25:26 +0100

    Added WindUtils class
    * Methods for wind conversion, wind barbs symbol, etc.

---
+++ src/utils/WindUtils.js
@@ -0,0 +1,45 @@
+/*
+* Smetak Wind Application
+* Copyright (c) 2015 Jan Simunek
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE.txt file in the root directory of this source tree.
+*/
+
+'use strict';
+
+module.exports = {
+
+  // Get updated time from date and time
+  getUpdatedTime:function(updatedDateTime) {
+    if (updatedDateTime === undefined) return '';
+    return updatedDateTime.substring(updatedDateTime.indexOf(' '));
+  },
+
+  // Get updated date from date and time
+  getUpdatedDate:function(updatedDateTime) {
+    if (updatedDateTime === undefined) return '';
+    return updatedDateTime.substring(0, updatedDateTime.indexOf(' '));
+  },
+
+  // Get wind barb symbol based on wind speed
+  // TODO: unit test
+  getWindBarbSymbol: function(windSpeed) {
+    if (! windSpeed) {
+      return String.fromCharCode(13);
+    }
+
+    var windSpeedKts = windSpeed * 1.9;
+    console.log(windSpeedKts);
+    var position = Math.floor(windSpeedKts / 5) + 33 - 1;
+    if (position < 33) position = 33;
+
+    return String.fromCharCode(position);
+  },
+
+  // Properly rotate windbarb symbol
+  getWindBarbDirection: function (windDirection) {
+    return windDirection;
+  }
+
+};
