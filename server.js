0fd11635521383daaa032a6db8deefe11073bc59
Jan Simunek <jan.simunek@ys.cz>
2015-03-16 10:19:34 +0100

    Prevent Your Heroku Node App From Sleeping
    * See https://quickleft.com/blog/6-easy-ways-to-prevent-your-heroku-node-app-from-sleeping/

--- server.js
+++ server.js
@@ -10,10 +10,18 @@
 
 var express = require('express');
 var app = express();
+var http = require('http');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/build'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running at localhost:' + app.get('port'));
+
+  // Keep application awake from sleeping
+  // TODO: Do not call this during development and get application URL from
+  // config, not hardcoded.
+  setInterval(function() {
+    http.get('http://smetak.herokuapp.com');
+  }, 300000); // every 5 minutes (300000)
 });
